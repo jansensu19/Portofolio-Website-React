81ff8de1eeec035df5d174c2bf6f9b985a1a47a8
Jansen Su <jansen.zix@gmail.com>
2022-08-05 04:42:10 +0700

    Adding new Git Logo and new Portofolio

--- src/Components/Portofolio/Portofolio.jsx
+++ src/Components/Portofolio/Portofolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Portofolio.css'
 import { Navigation, Pagination } from 'swiper'
-
+import Github from '@iconscout/react-unicons/icons/uil-github'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination';
@@ -29,28 +29,36 @@ const Portofolio = () => {
         modules={[Navigation, Pagination]}
         className='portofolio-slider'>
             <SwiperSlide>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/cRxkeUUkcPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <a href="https://jansen-portofolio-frontend-website.netlify.app/" className='web-portofolio'><small>Portofolio Web with React</small></a> <a href="https://github.com/jansensu19/Portofolio-Website-React"><Github color='black' size='2rem' className="git"/></a>
+            </SwiperSlide>
+            <SwiperSlide>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/hZQzjHCNdSk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <a href="https://superfinti.netlify.app/" className='web-portofolio'><small>Super Finti Dashboard With React</small></a> <a href="https://github.com/jansensu19/Dashboard-Super-Finti"><Github color='black' size='2rem' className="git"/></a>
+            </SwiperSlide>
+            <SwiperSlide>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ahoI_mN-30E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <a href="https://smartspace-eduaction.netlify.app/" className='web-portofolio'><small>Smart Space with React</small></a> <a href="https://github.com/jansensu19/SmartSpace"><Github color='black' size='2rem' className="git"/></a>
+            </SwiperSlide>
+            <SwiperSlide>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/Gv5otXNd_M0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://elearning-cuda.netlify.app/" className='web-portofolio'><small>Elearning With React</small></a>
-            </SwiperSlide>
-            <SwiperSlide>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/4MsqvUcUqW4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://cgs-ecommerce-web.netlify.app/" className='web-portofolio'><small>E-Commerce With Basic Web</small></a>
-            </SwiperSlide>
-            <SwiperSlide>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/epWWrK_EGMU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://slit.netlify.app/" className='web-portofolio'><small>Basic Web DiCoding Course</small></a>
+                <a href="https://elearning-cuda.netlify.app/" className='web-portofolio'><small>Elearning With React</small></a> <a href="https://github.com/jansensu19/E-Learning-Web-React-"><Github color='black' size='2rem' className="git"/></a>
             </SwiperSlide>
             <SwiperSlide>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/RMMQD1TUJAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://github.com/jansensu19/Dashboard" className='web-portofolio'><small>Dashboard ToDo with API (Angular)</small></a>
+                <a href="https://github.com/jansensu19/Dashboard" className='web-portofolio'><small>Dashboard ToDo with API (Angular)</small></a> <a href="https://github.com/jansensu19/Dashboard"><Github color='black' size='2rem' className="git"/></a>
             </SwiperSlide>
+            <SwiperSlide>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/4MsqvUcUqW4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <a href="https://cgs-ecommerce-web.netlify.app/" className='web-portofolio'><small>E-Commerce With Basic Web</small></a> <a href="https://github.com/jansensu19/CGS"><Github color='black' size='2rem' className="git"/></a>
+            </SwiperSlide>
+            <SwiperSlide>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/epWWrK_EGMU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <a href="https://slit.netlify.app/" className='web-portofolio'><small>Basic Web DiCoding Course</small></a> <a href="https://github.com/jansensu19/DiCoding-Basic-Web"><Github color='black' size='2rem' className="git"/></a>
+            </SwiperSlide> 
             <SwiperSlide>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/aNe5zdJ-O-M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://bookself-dom.netlify.app/" className='web-portofolio'><small>BookSelf DOM Front-End DiCoding Course</small></a>
-            </SwiperSlide>
-            <SwiperSlide>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/cRxkeUUkcPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <a href="https://jansen-portofolio-frontend-website.netlify.app/" className='web-portofolio'><small>Portofolio Web with React</small></a>
+                <a href="https://bookself-dom.netlify.app/" className='web-portofolio'><small>BookSelf DOM Front-End DiCoding Course</small></a> <a href="https://github.com/jansensu19/DiCodingFrontEnd"><Github color='black' size='2rem' className="git"/></a>
             </SwiperSlide>
         </Swiper>
     </div>
